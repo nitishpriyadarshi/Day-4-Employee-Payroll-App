5ff131406969e0cfc0fef9e7a6f76cfe756d87ba
nitis <nitish8152@gmail.com>
2022-12-20 19:54:13 +0530

    Added: determine the logging levels and console file

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -11,9 +11,9 @@ public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run
-				(EmployeePayrollAppApplication.class, args);
-		log.info("Employee PayRoll Application Started");
-		System.out.println("Welcome To Employee Payroll App");
+				(EmployeePayrollAppApplication.class,args);
+		log.info(" Hey!! Employee PayRoll App Started in {} Enviroment",
+				context.getEnvironment().getProperty("enviroment"));
 	}
 }
 
