e62d5b1cdfef88a82b9e05344ce2db3fda172458
nitis <nitish8152@gmail.com>
2022-12-22 12:40:52 +0530

    Added:provide user friendly error response in case validation fails

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exception/EmpPayRollException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayrollapp.exception;
+
+public class EmpPayRollException  extends RuntimeException{
+    public EmpPayRollException(String message){
+        super(message);
+    }
+}
